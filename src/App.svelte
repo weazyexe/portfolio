f48b5f436574a90143310d37b212019087fd8b8b
weazyexe <weazy.exe@gmail.com>
2020-04-06 17:56:45 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Router, Link, Route } from 'svelte-routing'
+	export let name;
+</script>
+
+<main>
+	<Router>
+		<Route  />
+	</Router>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
