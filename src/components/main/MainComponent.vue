4c9df6233f3a47d71a4d85c15cb47626e4f57052
Timur Zadvornov <weazy.exe@gmail.com>
2022-07-25 15:14:12 +0400

    removed all default vue stuff

---
+++ src/components/main/MainComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+const state = useCounterStore()
+</script>
+
+<template>
+    <main>
+        <div>Hello, World!</div>
+        <div> {{ state.doubleCount }} </div>
+        <button @click="state.increment()">Increment</button>
+    </main>
+</template>
